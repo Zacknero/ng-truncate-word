8987212158ac1b0641b6a500b833bfce04744657
Marco Rinaldi <marco.rinaldi9@live.com>
2024-09-12 19:30:38 +0200

    refactor library for using signal api and removed complexity

--- projects/truncate-word/src/lib/is-ellipsis-active.directive.ts
+++ projects/truncate-word/src/lib/is-ellipsis-active.directive.ts
@@ -1,63 +1,46 @@
 import {
-  AfterViewInit,
-  ChangeDetectorRef,
   Directive,
   ElementRef,
-  EventEmitter,
   inject,
-  Input,
-  Output,
-  Renderer2
+  model,
+  input,
+  computed,
+  effect,
+  untracked,
 } from '@angular/core';
 
 @Directive({
   selector: '[csiIsEllipsisActive]',
-  standalone: true
+  standalone: true,
+  host: {
+    '[title]': 'titleTrunc()',
+  },
 })
-export class IsEllipsisActiveDirective implements AfterViewInit {
-  el = inject(ElementRef);
-  cdr = inject(ChangeDetectorRef);
-  renderer = inject(Renderer2);
+export class IsEllipsisActiveDirective {
+  isTruncated = model<boolean>();
+  csiTruncateLabel = input.required<string>();
+  protected titleTrunc = computed(() =>
+    this.isTruncated() ? this.csiTruncateLabel() : ''
+  );
+  private readonly el = inject<ElementRef<HTMLDivElement>>(ElementRef);
 
-  @Output() isTruncatedChange: EventEmitter<boolean> =
-    new EventEmitter<boolean>();
-  @Input() csiTruncateLabel: string = '';
-  private _isTruncated: boolean = false;
-
-  @Input()
-  get isTruncated(): boolean {
-    return this._isTruncated;
+  constructor() {
+    // Effetto che "reagisce" all'input di csiTruncateLabel e controlla se deve troncare o meno il testo
+    effect(() => {
+      this.csiTruncateLabel();
+      untracked(() => this.checkTruncation());
+    });
   }
 
-  set isTruncated(value: boolean) {
-    this._isTruncated = value;
-    this.isTruncatedChange.emit(this._isTruncated);
-  }
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.checkTruncation();
-    }, 0);
-  }
-
-  checkTruncation(): void {
+  private checkTruncation(): void {
     const element = this.el.nativeElement;
 
     // Utilizza la funzione per controllare se il testo è troncato
     const isOverflowing = this.isTextOverflow(element);
-
-    if (isOverflowing) {
-      this.isTruncated = true;
-      this.renderer.setProperty(element, 'title', this.csiTruncateLabel);
-    } else {
-      this.isTruncated = false;
-    }
-
-    // Forza un nuovo ciclo di controllo delle modifiche
-    this.cdr.detectChanges();
+    this.isTruncated.set(isOverflowing);
   }
 
-  private isTextOverflow(element: HTMLElement): boolean {
+  private isTextOverflow(element: HTMLDivElement): boolean {
     return element.offsetWidth < element.scrollWidth;
   }
 }
